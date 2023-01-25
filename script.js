3dc1f9b9fcfb4961dc865bc95ee5291ef1b8e5fd
Nikhil Sethi <nikhil@Nikhils-MacBook-Pro.local>
2023-01-25 10:10:04 -0800

    updates to font, addition of plus, changing links

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const toggleRow = (element) => {
             element.getElementsByClassName("expanded-row-content").item(0).classList.toggle("hide-row");
+            element.getElementsByClassName("plus-icon").item(0).classList.toggle("x-icon");
          }
